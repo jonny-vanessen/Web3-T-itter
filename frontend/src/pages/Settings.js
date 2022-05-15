8e9b1731cd5ec60d4cd64201e20daf75e1612815
Development <Development@Jonnys-MacBook-Pro-2.local>
2022-05-15 14:28:12 -0500

    add nft as pfp

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,25 +1,51 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import './Settings.css';
 import { Input } from 'web3uikit';
-import pfp1 from '../images/pfp1.png';
-import pfp2 from '../images/pfp2.png';
-import pfp3 from '../images/pfp3.png';
-import pfp4 from '../images/pfp4.png';
-import pfp5 from '../images/pfp5.png';
+// import pfp1 from '../images/pfp1.png';
+// import pfp2 from '../images/pfp2.png';
+// import pfp3 from '../images/pfp3.png';
+// import pfp4 from '../images/pfp4.png';
+// import pfp5 from '../images/pfp5.png';
 import { defaultImgs } from '../defaultImgs';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useMoralisWeb3Api } from 'react-moralis';
 
 
 const Settings = () => {
 
-  const pfps = [pfp1, pfp2, pfp3, pfp4, pfp5];
+  const [pfps, setPfps] = useState([]);
   const [selectedPfp, setSelectedPfp] = useState();
   const inputFile = useRef(null);
   const [selectedFile, setSelectedFile] = useState(defaultImgs[1]);
   const [theFile, setTheFile] = useState();
   const [username, setUsername] = useState();
   const [bio, setBio] = useState();
-  const { Moralis } = useMoralis();
+  const { Moralis, isAuthenticated, account } = useMoralis();
+  const Web3Api = useMoralisWeb3Api();
+
+
+  const resolveLink = (url) => {
+    if (!url || !url.includes('ipfs://')) return url;
+    return url.replace('ipfs://', 'https://gateway.ipfs.io/ipfs/');
+  };
+
+  useEffect(() => {
+    const getNFTs = async () => {
+
+      const options = {
+        chain: 'mumbai',
+        address: account
+      };
+
+      const mumbaiNFTs = await Web3Api.account.getNFTs(options);
+
+      const images = mumbaiNFTs.result.map((nft) => {
+        return resolveLink(JSON.parse(nft.metadata)?.image);
+      });
+      setPfps(images);
+    };
+    getNFTs();
+  }, [isAuthenticated, account, Web3Api.account]);
+
 
   const saveEdits = async () => {
     const User = Moralis.Object.extend('_User');
@@ -32,6 +58,9 @@ const Settings = () => {
     }
     if (bio) {
       myDetails.set('bio', bio);
+    }
+    if (selectedPfp) {
+      myDetails.set('pfp', selectedPfp);
     }
 
     if (theFile) {
